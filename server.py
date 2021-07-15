99ed76d02d22b6f170cc59cfc0d101dc0b0c6792
GuildGamer <tobi4steve@gmail.com>
2021-07-16 00:48:37 +0100

    changes

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from waitress import serve
+import os
     
 from glc.wsgi import application
     
 if __name__ == '__main__':
-    serve(application)
+    port = os.getenv('PORT', default=8000)
+    serve(application, port=port)
 
 threads = 8
+
+
