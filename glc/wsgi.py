364f2c660795cdef2b207387cd9223fa41a8a612
GuildGamer <tobi4steve@gmail.com>
2021-07-15 08:09:31 +0100

    'server'

--- glc/wsgi.py
+++ glc/wsgi.py
@@ -4,4 +4,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glc.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
+application = WhiteNoise(get_wsgi_application())
+
