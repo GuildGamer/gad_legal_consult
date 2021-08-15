e2689d29d6f433e77cb0ff5b593faeece354f266
GuildGamer <tobi4steve@gmail.com>
2021-08-15 22:35:32 +0100

    changes to session model

--- base/email.py
+++ base/email.py
@@ -6,8 +6,10 @@ qs = Session.objects.all().filter(sent=False)
 
 server = smtplib.SMTP_SSL("smpt.gmail.com", 465)
 server.login("tobi4steve@gmail.com", "password")
-for user in qs:
+for session_request in qs:
     server.sendmail("tobi4steve@gmail.com",
-                    user.email,
+                    session_request.email,
                     "Your booking was sucessful! await a response from Victor Momodu. cheers!")
+
+    session_request.sent = True
 server.quit()
